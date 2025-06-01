bbad2e910a75b2dde339a65a5339e28a3ad8a259
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-01 17:53:32 +0100

    Generate random die

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,21 @@ import Die from "./Die";
 
 function App() {
 
+  const [dice, setDice] = useState(generateAllNewDice());
+
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
-      .map(() => Math.ceil(Math.random() * 100));
+      .map(() => Math.ceil(Math.random() * 6));
   }
+
+  const diceElements = dice.map(dieValue => <Die value={dieValue} />);
 
   return (
     <>
       <main>
         <div className="dice-holder">
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
+          {diceElements}
         </div>
       </main>
     </>
