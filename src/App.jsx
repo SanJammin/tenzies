bb28a6ba2a526b07417402f673da8b0ae134843e
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-01 17:34:45 +0100

    Create dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import Die from "./Die";
 
 function App() {
 
   return (
     <>
       <main>
-        
+        <div className="dice-holder">
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+        </div>
       </main>
     </>
   );
