5977adb10eddafcd770026f9384401448b775b46
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-02 19:55:10 +0100

    Improve readability

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,9 @@ function App() {
     <>
       <main>
         <h1 className="title">Tenzies</h1>
-        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+        </p>
         <div className="dice-holder">
           {diceElements}
         </div>
