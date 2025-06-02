dd2036b20fdf51384ea9b22467e74d6894c9647c
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-02 19:11:21 +0100

    Add styling to held die

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,5 +1,9 @@
 export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#fff"
+    }
+
     return (
-        <div className="die">{props.value}</div>
+        <div className="die" style={styles}>{props.value}</div>
     );
 }
